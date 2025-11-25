777a66358cca75e9752e4fb74f0c430ceffcb169
Dominic Chen <cchendominic@gmail.com>
2025-11-25 10:42:15 -0500

    Configure UI with HTML and TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let addEl = document.getElementById("add-el") as HTMLButtonElement
+let inputEl = document.getElementById("input-el") as HTMLInputElement
+let taskEl = document.getElementById("task-el") as HTMLUListElement
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let tasks: string[] = []
+
